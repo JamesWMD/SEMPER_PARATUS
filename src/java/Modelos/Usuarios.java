a46202d6124de11103601a8b0b7cf289938a4080
JAMES WILLIAM MONTENEGRO DAZA <jwmontenegrodaza@gmail.com>
2025-02-01 12:08:21 -0500

    Se creao el packege (Utilidades) y dentro la clase (ConexionDB)

--- src/java/Modelos/Usuarios.java
+++ src/java/Modelos/Usuarios.java
@@ -1,13 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelos;
 
-/**
- *
- * @author JAMES
- */
 public class Usuarios {
+    private int idUsuarios;
+    private String noIdentificacion;
+    private String nombreUsuario;
+    private String password;
+    private String estado;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(String noIdentificacion, String nombreUsuario, String password, String estado) {
+        this.noIdentificacion = noIdentificacion;
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+        this.estado = estado;
+    }
+
+    public Usuarios(int idUsuarios, String noIdentificacion, String nombreUsuario, String password, String estado) {
+        this.idUsuarios = idUsuarios;
+        this.noIdentificacion = noIdentificacion;
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+        this.estado = estado;
+    }
+
+    public int getIdUsuarios() {
+        return idUsuarios;
+    }
+
+    public void setIdUsuarios(int idUsuarios) {
+        this.idUsuarios = idUsuarios;
+    }
+
+    public String getNoIdentificacion() {
+        return noIdentificacion;
+    }
+
+    public void setNoIdentificacion(String noIdentificacion) {
+        this.noIdentificacion = noIdentificacion;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
     
 }
